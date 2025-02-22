776cdbaccfd19816ba705d7e2c7f64208ceb32b9
Timothy Stewart <timothystewart6@gmail.com>
2025-02-22 10:58:28 -0600

    chore(dev): Updated deploy script to install a release version of the app and run in the background

--- Makefile
+++ Makefile
@@ -11,6 +11,9 @@ build_dev: hash_resource
 frontend:
 	cd ui && npm ci && npm run build:device
 
+frontend_prod:
+	cd ui && npm ci && npm run build:prod
+
 dev_release: build_dev
 	@echo "Uploading release..."
 	@shasum -a 256 bin/jetkvm_app | cut -d ' ' -f 1 > bin/jetkvm_app.sha256
@@ -31,3 +34,8 @@ release:
 	@shasum -a 256 bin/jetkvm_app | cut -d ' ' -f 1 > bin/jetkvm_app.sha256
 	rclone copyto bin/jetkvm_app r2://jetkvm-update/app/$(VERSION)/jetkvm_app
 	rclone copyto bin/jetkvm_app.sha256 r2://jetkvm-update/app/$(VERSION)/jetkvm_app.sha256
+
+
+build_release_prod: frontend_prod hash_resource
+	@echo "Building release..."
+	GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-s -w -X kvm.builtAppVersion=$(VERSION)" -o bin/jetkvm_app cmd/main.go
--- dev_deploy.sh
+++ dev_deploy.sh
@@ -10,17 +10,20 @@ show_help() {
     echo
     echo "Optional:"
     echo "  -u, --user <remote_user>   Remote username (default: root)"
+    echo "  -i, --install              Install the release version (jetkvm_app) instead of the debug version (jetkvm_app_debug)"
     echo "      --help                 Display this help message"
     echo
     echo "Example:"
     echo "  $0 -r 192.168.0.17"
     echo "  $0 -r 192.168.0.17 -u admin"
+    echo "  $0 -r 192.168.0.17 -i"
     exit 0
 }
 
 # Default values
 REMOTE_USER="root"
 REMOTE_PATH="/userdata/jetkvm/bin"
+INSTALL_RELEASE=false
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -32,6 +35,10 @@ while [[ $# -gt 0 ]]; do
         -u|--user)
             REMOTE_USER="$2"
             shift 2
+            ;;
+        -i|--install)
+            INSTALL_RELEASE=true
+            shift 1
             ;;
         --help)
             show_help
@@ -51,18 +58,29 @@ if [ -z "$REMOTE_HOST" ]; then
     show_help
 fi
 
-# Build the development version on the host
+# Build the appropriate version on the host
 make frontend
-make build_dev
+if [ "$INSTALL_RELEASE" = true ]; then
+    make build_release_prod
+else
+    make build_dev
+fi
 
 # Change directory to the binary output directory
 cd bin
 
+# Determine the binary to deploy
+if [ "$INSTALL_RELEASE" = true ]; then
+    BINARY_NAME="jetkvm_app"
+else
+    BINARY_NAME="jetkvm_app_debug"
+fi
+
 # Kill any existing instances of the application
-ssh "${REMOTE_USER}@${REMOTE_HOST}" "killall jetkvm_app_debug || true"
+ssh "${REMOTE_USER}@${REMOTE_HOST}" "killall $BINARY_NAME || true"
 
 # Copy the binary to the remote host
-cat jetkvm_app | ssh "${REMOTE_USER}@${REMOTE_HOST}" "cat > $REMOTE_PATH/jetkvm_app_debug"
+cat "$BINARY_NAME" | ssh "${REMOTE_USER}@${REMOTE_HOST}" "cat > $REMOTE_PATH/$BINARY_NAME"
 
 # Deploy and run the application on the remote host
 ssh "${REMOTE_USER}@${REMOTE_HOST}" ash <<EOF
@@ -79,10 +97,14 @@ killall jetkvm_app_debug || true
 cd "$REMOTE_PATH"
 
 # Make the new binary executable
-chmod +x jetkvm_app_debug
+chmod +x $BINARY_NAME
 
 # Run the application in the background
-PION_LOG_TRACE=jetkvm,cloud ./jetkvm_app_debug
+if [ "$INSTALL_RELEASE" = true ]; then
+    ./$BINARY_NAME &
+else
+    PION_LOG_TRACE=jetkvm,cloud ./$BINARY_NAME
+fi
 EOF
 
 echo "Deployment complete."
